9d39c6b249aed007d9d15d5528266e337819ca67
fionadonohue <97976082+fionadonohue@users.noreply.github.com>
2022-01-27 09:58:28 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
+	$("#p1").click(function() {
+		$(".disappear").hide() ;
+	});
+
 	$("h1").click(function() {
 		$("p").css({"color":"red", "background-color": "yellow"});
 		$("img").css("border","25px solid black");
 	});
 	$("#imagetoggler").click(function(){
 		$("img").toggle();
-
-		$(document).ready(function() {
-			$("p1").click(function() {
-				$("class=dissapear").hide()
 	});
+	});
+
+$("p").click(function(){
+	alert("hey! don't click me!");
+});
